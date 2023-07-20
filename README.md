c1531d8d9a4020a8ee386b581ac667725a2b7f1f
Jean-Louis Voiseux <48380853+jlvoiseux@users.noreply.github.com>
2023-07-20 01:38:07 +0100

    Added normal maps

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ cd "C:\Program Files\Blender Foundation\Blender 3.4\3.4\python\bin"
 .\python.exe -m pip install --upgrade pip
 .\python.exe -m pip install opensimplex
 ```
+
+TODO
+- Vectorized multiplications
+- Humidity and elevation
+- Seamless noise
+- Export to file
+- Small UI
--- script.py
+++ script.py
@@ -36,7 +36,7 @@ def purge_all():
         
         
 def fractalized_opensimplex_noise2(x, y):
-    num_octaves = 4
+    num_octaves = 8
     frequency = 0.05
     amplitude = 1
     lacunarity = 2.0
@@ -50,7 +50,7 @@ def fractalized_opensimplex_noise2(x, y):
     return n
         
         
-def generate_procedural_texture(texture_size):
+def generate_height_map(texture_size):
     noise_texture = []
     for i in range(texture_size):
         row = []
@@ -69,19 +69,42 @@ def generate_procedural_texture(texture_size):
     return image
 
 
-def create_large_textured_sphere(texture):
-    bpy.context.scene.cursor.location = (0, 0, 0)
-    # Add a new UV sphere mesh with 256 slices
-    bpy.ops.mesh.primitive_uv_sphere_add(segments=256, ring_count=256)
+def generate_normal_map(height_map, texture_size):
+    bpy.ops.mesh.primitive_plane_add(size=2)
+    plane = bpy.context.active_object
+    bpy.ops.object.material_slot_add()
+    plane.material_slots[0].material = bpy.data.materials.new(name="NormalMaterial")
+    
+    mat = plane.material_slots[0].material
+    mat.use_nodes = True
+    nodes = mat.node_tree.nodes
+    nodes.clear()
+    links = mat.node_tree.links
+    links.clear()
+    
+    texture_node = nodes.new(type="ShaderNodeTexImage")
+    texture_node.image = height_map
+    bump_node = nodes.new(type="ShaderNodeBump")
+    bsdf_node = nodes.new(type='ShaderNodeBsdfDiffuse')
+    output_node = nodes.new(type="ShaderNodeOutputMaterial")
+    
+    links.new(texture_node.outputs["Color"], bump_node.inputs["Normal"])
+    links.new(bump_node.outputs["Normal"], output_node.inputs["Displacement"])
+    links.new(bsdf_node.outputs["BSDF"], output_node.inputs["Surface"])
+    
+    baked_normal_node = nodes.new(type="ShaderNodeTexImage")
+    baked_normal_node.image = bpy.data.images.new("BakedNormal", width=texture_size, height=texture_size)
+    baked_normal_node.select = True
+    nodes.active = baked_normal_node
+    bpy.ops.object.select_all(action="SELECT")
+    bpy.ops.object.bake(type="NORMAL")
+    bpy.data.objects.remove(plane)
+    
+    return baked_normal_node.image
+    
 
-    # Get the newly created sphere object
-    sphere = bpy.context.active_object
-
-    # Add a new material to the sphere
+def generate_final_material(height_map, normal_map):
     mat = bpy.data.materials.new(name="SphereMaterial")
-    sphere.data.materials.append(mat)
-
-    # Assign the texture to the material
     mat.use_nodes = True
     nodes = mat.node_tree.nodes
     nodes.clear()
@@ -89,8 +112,14 @@ def create_large_textured_sphere(texture):
     links.clear()
     
     # Height map
-    tex_node = nodes.new(type='ShaderNodeTexImage')
-    tex_node.image = texture
+    height_map_node = nodes.new(type='ShaderNodeTexImage')
+    height_map_node.image = height_map
+    
+    # Normal map
+    normal_map_node = nodes.new(type='ShaderNodeTexImage')
+    normal_map_node.image = normal_map
+    
+    normal_node = nodes.new(type='ShaderNodeNormalMap')
     
     color_ramp_node = nodes.new(type='ShaderNodeValToRGB')
     color_ramp_node.color_ramp.interpolation = 'CONSTANT'
@@ -105,18 +134,36 @@ def create_large_textured_sphere(texture):
     color_ramp_node.color_ramp.elements[4].color = (1.0, 1.0, 1.0, 1.0)
     color_ramp_node.color_ramp.elements[5].color = (1.0, 1.0, 1.0, 1.0)
     
-    diffuse_node = nodes.new(type='ShaderNodeBsdfDiffuse')
+    bsdf_node = nodes.new(type='ShaderNodeBsdfPrincipled')
     
     output_node = nodes.new(type='ShaderNodeOutputMaterial')
     
-    links.new(tex_node.outputs['Color'], color_ramp_node.inputs['Fac'])
-    links.new(color_ramp_node.outputs['Color'], diffuse_node.inputs['Color'])
-    links.new(diffuse_node.outputs['BSDF'], output_node.inputs['Surface'])
+    # Height map path
+    links.new(height_map_node.outputs['Color'], color_ramp_node.inputs['Fac'])
+    links.new(color_ramp_node.outputs['Color'], bsdf_node.inputs['Base Color'])
+    links.new(bsdf_node.outputs['BSDF'], output_node.inputs['Surface'])
+    
+    # Normal map path
+    links.new(normal_map_node.outputs['Color'], normal_node.inputs['Color'])
+    links.new(normal_node.outputs['Normal'], bsdf_node.inputs['Normal'])
 
-    # Assign the material to the sphere
+    return mat
+
+    
+def generate_final_sphere(size, mat):
+    bpy.context.scene.cursor.location = (0, 0, 0)
+    bpy.ops.mesh.primitive_uv_sphere_add(segments=size, ring_count=size)
+    sphere = bpy.context.active_object
+    sphere.data.materials.append(mat)
     sphere.data.materials[0] = mat
+    
 
 if __name__ == "__main__":
     purge_all()
-    texture = generate_procedural_texture(256)
-    create_large_textured_sphere(texture)
+    bpy.context.scene.render.engine = 'CYCLES'
+    maps_size = 2048
+    height_map = generate_height_map(maps_size)
+    normal_map = generate_normal_map(height_map, maps_size)
+    bpy.context.scene.render.engine = 'BLENDER_EEVEE'
+    mat = generate_final_material(height_map, normal_map)
+    generate_final_sphere(256, mat)
